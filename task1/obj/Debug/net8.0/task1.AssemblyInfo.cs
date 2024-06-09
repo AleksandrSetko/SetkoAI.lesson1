8aa73e677e8eb0f50c3adf50ae0ebe8e51a8a2b0
Setko AI <setko@msn.com>
2024-06-09 14:04:15 +0300

    homework

--- task1/obj/Debug/net8.0/task1.AssemblyInfo.cs
+++ task1/obj/Debug/net8.0/task1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("task1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ed217f532907839f346370bb6663e806407eeff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7ddcdbf1fffb9c3d4928f769ffc869322ee64a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("task1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("task1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
